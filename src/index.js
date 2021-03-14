eb85b6b6d794f36563c58b4d52da01a582b0b8d6
anastasiaasadcheva94 <asadchevaanastasia@gmail.com>
2021-03-14 17:07:38 +0300

    init: task Typical Arrays Problems complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  } else {
+    var minNum = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (minNum > array[i]) minNum = array[i]; 
+  }
+    return minNum;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  } else {
+  var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i]; 
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0){
+    return 0;
+  } else {
+    var avg = 0;
+    for (var i = 0; i < array.length; i++) {
+      avg += array[i];
+  }
+  return avg/array.length;
+  }
 }
